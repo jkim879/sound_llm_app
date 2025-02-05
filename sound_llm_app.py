5648a57a3cc1edebd443116d0b84485f0c977693
Jiseong Kim <54978199+jkim879@users.noreply.github.com>
2025-02-05 16:27:31 +0900

    Update sound_llm_app.py

--- sound_llm_app.py
+++ sound_llm_app.py
@@ -267,7 +267,7 @@ if uploaded_file:
                                 요약 내용의 길이는 반드시 {st.session_state['settings']['summary_length']}에서 해결하세요.
                                 대답은 반드시 요약 형식으로 답변.
                                 전체적인 내용을 한 번 더 파악해서 오타나 오탈자 수정.
-                                인물에 집중하지 말고 상황에 집중해서 가독성 좋게 보고서 형식으로 요약.
+                                인물에 집중하지 말고 상황에 집중해서 가독성 좋게 보고서 형식으로 요약. 1,2,3,4 사용해서 맥락 구분 중요.
                             """}
                         ],
                         max_tokens=st.session_state['settings']['summary_length'],
