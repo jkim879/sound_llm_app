eacd725a622e97773bdb56fa894ccee8dfb5179e
Jiseong Kim <54978199+jkim879@users.noreply.github.com>
2025-02-11 17:18:34 +0900

    Update sound_llm_app.py

--- sound_llm_app.py
+++ sound_llm_app.py
@@ -197,7 +197,7 @@ if uploaded_file:
         # Transcription
         with st.spinner("음성 변환 중..."):
             transcription = client.audio.transcriptions.create(
-                model= "turbo",
+                model= "whisper-1",
                 file=uploaded_file,
                 response_format="text"
             )
